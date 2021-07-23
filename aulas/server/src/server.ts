8ec287e27cfeb25e23d5a0ada4b8ed014a01e096
Estyvison Linhares de Albuquerque <39842092+EstyvisonLinhares@users.noreply.github.com>
2021-07-23 15:15:32 -0300

    serverJs antes começo aula 2

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,17 +1,41 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+app.use(express.json()); //Colocar uma funcionalidade para o express entender o corpo da nossa requisição em formato json
 
-    response.json([
-        'Tito',
-        'Teixa',
-        'Robs',
-        'Tin',
-        'Caca'
-    ]);
+const users = [
+    'Tito',
+    'Teixa',
+    'Robson',
+    'Tin',
+    'Caca',
+    'Estyvison'
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search); //Não façam isso em casa crianças!
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    return response.json(users[id])
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user); //return antes do response para que a requisição se encerre aqui
 });
 
 app.listen(3333);
